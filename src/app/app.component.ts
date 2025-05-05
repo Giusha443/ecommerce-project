54b67d8361f48c7ea84b4466806a48db44941133
pashay94 <pascom_by@mail.ru>
2025-05-05 19:44:52 +0300

    fix: interseptors style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ApiService } from '../services/api.service';
-import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-root',
@@ -9,35 +7,6 @@ import { StorageService } from '../services/storage.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'ecommerce-project';
-  constructor(
-    private api: ApiService,
-    private store: StorageService
-  ) {}
-  ngOnInit() {
-    this.getData();
-    setTimeout(() => {
-      this.getData2();
-    }, 3000);
-  }
-  async getData() {
-    try {
-      const res = this.api.getAuthToken().subscribe(data => {
-        this.store.setAccessToken(data.access_token);
-      });
-      console.log(res);
-    } catch (e) {
-      console.error('Error fetching customers:', e);
-    }
-  }
-  async getData2() {
-    try {
-      this.api.getProducts().subscribe(data => {
-        console.log(data);
-      });
-    } catch (e) {
-      console.error('Error fetching customers:', e);
-    }
-  }
 }
