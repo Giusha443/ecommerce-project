cb5c9e70d0bedb7e1da20bb9c8ec2ecab0b40dd6
Denis.Karev <goofy.adun@gmail.com>
2025-05-12 21:27:52 +0300

    feat: (ECOMM-38) add routing and repair some tests

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../pages/login/login.component';
-import { RegisterComponent } from '../pages/register/register.component';
+import { NotFoundComponent } from '../pages/not-found/not-found.component';
+// import { AuthGuard } from '../guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: RegisterComponent },
-  // { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadComponent: () => import('../pages/login/login.component').then(l => l.LoginComponent),
+    // canActivate: [AuthGuard],
+  },
+  {
+    path: 'registration',
+    loadComponent: () => import('../pages/register/register.component').then(r => r.RegisterComponent),
+  },
+  {
+    path: 'main',
+    loadComponent: () => import('../pages/main/main.component').then(main => main.MainComponent),
+  },
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
 ];
