a919b797fc0ecffc2ed3a2ce98785e364c185dba
Denis.Karev <goofy.adun@gmail.com>
2025-05-18 17:13:25 +0300

    fix: fix/feat add redirect for github (i hope) and dinamic title to pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from '../pages/not-found/not-found.component';
 import { authGuard, loginGuard } from '../guards/auth.guard';
+import { APP_TITLE } from '../constants/app.title';
 import { LogOutComponent } from '../components/log-out/log-out.component';
 
 export const routes: Routes = [
@@ -8,11 +9,13 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('../pages/login/login.component').then(l => l.LoginComponent),
     canActivate: [loginGuard],
+    title: `Login - ${APP_TITLE}`,
   },
   {
     path: 'signup',
     loadComponent: () => import('../pages/register/register.component').then(r => r.RegisterComponent),
     canActivate: [loginGuard],
+    title: `Register - ${APP_TITLE}`,
   },
   {
     path: 'main',
@@ -20,10 +23,13 @@ export const routes: Routes = [
   },
   {
     path: 'logout',
-    title: `logged out`,
     component: LogOutComponent,
     canActivate: [authGuard],
   },
   { path: '', redirectTo: 'main', pathMatch: 'full' },
-  { path: '**', component: NotFoundComponent },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    title: `404 not found - ${APP_TITLE}`,
+  },
 ];
