56add81d69cc45657860f9cdafaa3dacf1f947a8
Giusha443 <giorgi.jajanidze2003@gmail.com>
2025-05-20 00:11:01 +0400

    fix: change guards

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,39 +1,25 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { skip, tap } from 'rxjs';
 
-/**
- * Auth guard to protect routes from unauthorized access
- * Redirects to login page if user is not authenticated
- */
 export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  authService.checkLoginStatus();
-  return authService.isAuthenticated$.pipe(
-    skip(1),
-    tap(isAuthenticated => {
-      if (isAuthenticated) {
-        router.navigate(['main']);
-      }
-    })
-  );
+
+  if (authService.isAuthenticated$.getValue()) {
+    return true;
+  }
+  router.navigate(['login']);
+  return false;
 };
 
-/**
- * Login guard to prevent authenticated users from accessing login page
- * Redirects to main page if user is already authenticated
- */
 export const loginGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  return authService.isAuthenticated$.pipe(
-    skip(1),
-    tap(isAuthenticated => {
-      if (isAuthenticated) {
-        router.navigate(['main']);
-      }
-    })
-  );
+
+  if (authService.isAuthenticated$.getValue()) {
+    router.navigate(['main']);
+    return false;
+  }
+  return true;
 };
