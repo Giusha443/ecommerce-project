0855c7e25d99a04bf85021097fb97d4e5a6b486b
Denis.Karev <goofy.adun@gmail.com>
2025-05-19 22:45:12 +0300

    feat: fix authGuard attempt 2

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -14,6 +14,23 @@ export const authGuard: CanActivateFn = () => {
   return authService.isAuthenticated$.pipe(
     skip(1),
     tap(isAuthenticated => {
+      if (!isAuthenticated) {
+        router.navigate(['main']);
+      }
+    })
+  );
+};
+
+/**
+ * Login guard to prevent authenticated users from accessing login page
+ * Redirects to main page if user is already authenticated
+ */
+export const loginGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  return authService.isAuthenticated$.pipe(
+    skip(1),
+    tap(isAuthenticated => {
       if (isAuthenticated) {
         router.navigate(['main']);
       }
@@ -24,31 +41,14 @@ export const authGuard: CanActivateFn = () => {
 /**
  * Login guard to prevent authenticated users from accessing login page
  * Redirects to main page if user is already authenticated
-//  */
+ */
 // export const loginGuard: CanActivateFn = () => {
 //   const authService = inject(AuthService);
 //   const router = inject(Router);
-//   return authService.isAuthenticated$.pipe(
-//     skip(1),
-//     tap(isAuthenticated => {
-//       if (isAuthenticated) {
-//         router.navigate(['main']);
-//       }
-//     })
-//   );
+
+//   if (authService.isAuthenticated$.getValue()) {
+//     router.navigate(['main']);
+//     return false;
+//   }
+//   return true;
 // };
-
-/**
- * Login guard to prevent authenticated users from accessing login page
- * Redirects to main page if user is already authenticated
- */
-export const loginGuard: CanActivateFn = () => {
-  const authService = inject(AuthService);
-  const router = inject(Router);
-
-  if (authService.isAuthenticated$.getValue()) {
-    router.navigate(['main']);
-    return false;
-  }
-  return true;
-};
