8c556be4b15877bd91ac5f5a30d271706e58dc86
pashaBy94 <pascom_by@mail.ru>
2025-06-16 22:31:36 +0300

    fix: delete contact as route

--- src/constants/menu-items.ts
+++ src/constants/menu-items.ts
@@ -3,7 +3,6 @@ import { MenuItem } from '../models/types';
 export const MENU_ITEMS: MenuItem[] = [
   { item: 'Catalog', route: 'catalog' },
   { item: 'Search', route: 'search' },
-  { item: 'Contact Us', route: 'contact' },
   { item: 'Cart', route: 'cart' },
   { item: 'About Us', route: 'about' },
 ];
