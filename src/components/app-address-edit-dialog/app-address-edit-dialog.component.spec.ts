d690b4431bf1cfc83a09e431a94e7e0c90281dcf
DenisKarev <108752013+DenisKarev@users.noreply.github.com>
2025-06-16 22:05:16 +0300

    fix/tests 19/19 (#43)

--- src/components/app-address-edit-dialog/app-address-edit-dialog.component.spec.ts
+++ src/components/app-address-edit-dialog/app-address-edit-dialog.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { AddressEditDialogComponent } from './app-address-edit-dialog.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('AppAddressEditDialogComponent', () => {
   let component: AddressEditDialogComponent;
@@ -8,7 +9,12 @@ describe('AppAddressEditDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AddressEditDialogComponent],
+      imports: [AddressEditDialogComponent, MatDialogModule],
+      providers: [
+        provideHttpClient(),
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: { id: '123' } },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddressEditDialogComponent);
